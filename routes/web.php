7821cc13efb60420107a182aa515517710916f3f
ERaufi <79995615+ERaufi@users.noreply.github.com>
2024-04-01 00:02:59 +0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Events\testingEvent;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('test', function () {
+
+    event(new testingEvent('hello from Web.php'));
+
+    return 'done';
+});
